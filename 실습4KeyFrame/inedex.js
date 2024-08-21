14d1eedea53215955b50745fa574c42ec00e14ab
Gyo <oey157@nate.com>
2024-08-21 13:38:10 +0900

    클릭시 점프 추가

--- 실습4KeyFrame/inedex.js
+++ 실습4KeyFrame/inedex.js
@@ -2,18 +2,22 @@
 // 	document.getElementById("")
 // });
 
-('.content').addEventListener("click", (e) => {
-    if (!stopflag) {
-    ('.content').classList.add("jump");
-      stopflag = true;
-      jumpNumCnt.textContent++;
-    } else if (stopflag) {
-      return;
-    }
-  
+// ('.content').addEventListener("click", (e) => {
+//     if (!stopflag) {
+//     ('.content').classList.add("jump");
+//       stopflag = true;
+//       jumpNumCnt.textContent++;
+//     } else if (stopflag) {
+//       return;
+//     }
+document.querySelector('.content').addEventListener('click', function() {
+  const content = this;
+
+  // jump 클래스 추가
+  content.classList.add('jump');
     // 연속으로 캐릭터가 뛰지 않도록 조절
-    setTimeout(() => {
-    ('.content').classList.remove("jump");
-      stopflag = false;
-    }, 1000);
+    setTimeout(function() {
+    content.classList.remove("jump");
+    content = false;
+    }, 500);
   });
